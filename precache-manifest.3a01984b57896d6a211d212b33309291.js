02d4b6913fefb6fb3298535112be6584bb2baa82
Adil Mian <adilmian95@gmail.com>
2020-10-13 12:15:59 +0300

    Updates

---
+++ precache-manifest.3a01984b57896d6a211d212b33309291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644ec5731ab26325e1696ea9f9f827bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdf9aa2675e58020655a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4946ce0a9603be89b306",
+    "url": "/static/js/2.e4286e9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4286e9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf9aa2675e58020655a",
+    "url": "/static/js/main.131e3cf6.chunk.js"
+  },
+  {
+    "revision": "58680c34e958a67a7539",
+    "url": "/static/js/runtime-main.9eb9b5b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
