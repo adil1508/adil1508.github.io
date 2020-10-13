7cb3d4b29886493571baf4e2ae5594f6fe580986
Adil Mian <adilmian95@gmail.com>
2020-10-13 14:24:54 +0300

    humble beginnings

---
+++ reactified/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../logo.svg";
+import "./Home.css";
+
+const Home = () => {
+	return (
+		<div className='background'>
+			<p>hi, i'm adil</p>
+			<p>i like to code</p>
+		</div>
+	);
+};
+
+export default Home;
