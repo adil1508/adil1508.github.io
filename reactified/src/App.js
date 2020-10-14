ebaa21e1b881f9a4302265846aad02bdc807e511
Adil Mian <adilmian95@gmail.com>
2020-10-14 20:38:34 +0300

    ready for publish v0.1

--- reactified/src/App.js
+++ reactified/src/App.js
@@ -5,13 +5,12 @@ import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 import Me from "./components/Me";
-import Project from "./components/Project";
 import { HashRouter, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<HashRouter>
-			<Route exact path='/' component={Me} />
+			<Route exact path='/' component={Home} />
 			<Route exact path='/projects' component={Projects} />
 			<Route exact path='/resume' component={Resume} />
 			<Route exact path='/me' component={Me} />
