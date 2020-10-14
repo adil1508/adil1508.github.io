2ca51affd006a4a9669854e93c53cd3ef11be315
Adil Mian <adilmian95@gmail.com>
2020-10-14 10:52:43 +0300

    landing page done

--- reactified/src/components/Home.js
+++ reactified/src/components/Home.js
@@ -1,12 +1,35 @@
 import React from "react";
-import logo from "../logo.svg";
+import { withStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
 import "./Home.css";
+
+const ColorButton = withStyles(() => ({
+	root: {
+		color: "#fff",
+		margin: "0.5em",
+		paddingInline: "1em",
+		width: "10em",
+		fontFamily: "Helvetica",
+		fontSize: "1vw",
+		"&:hover": {
+			backgroundColor: "#1D2C34",
+		},
+	},
+}))(Button);
 
 const Home = () => {
 	return (
-		<div className='App-header'>
-			<img src={logo} className='App-logo' alt='logo' />
-			<p>React-ification in process</p>
+		<div className='root-container'>
+			<div className='main-text-container'>
+				<p className='main-text'>hi, i'm adil</p>
+				<p className='main-text'>i like to code</p>
+			</div>
+			<div className='navigation'>
+				<ColorButton>Projects</ColorButton>
+				<ColorButton>Resume</ColorButton>
+				<ColorButton>Me!</ColorButton>
+			</div>
 		</div>
 	);
 };
